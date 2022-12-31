06501a469a3cc2bb9a7e0d098c13aaf33042262a
MnhMG <121704748+MnhMG@users.noreply.github.com>
2022-12-31 13:17:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'OPEN';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100007355536314';
+const eventName = '(˃ᆺ˂)Click to';
+const titleCard = 'Dear My Queen';
+const contentCard = 'Chúc Mừng Năm Mới, tạm biệt 2022 với nhiều tiếc nuối, 2023 hãy tỏa sáng rực rỡ nhé. Mong cầu mọi điều tốt đẹp sẽ tới với Ngô Thanh Huyền trong năm tới. Cũng cảm ơn 2022 đã cho tôi được gặp người. Một lần nữa, cảm ơn rất nhiều. Hãy cùng cố gắng ở 2023 và những năm tới na❤️' ;
+
+const giftImage = 'NTH.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
